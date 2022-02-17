f0cea4809c285fa11faf386d2f91beb9d374dc31
Peter Gemmell <peter.gemmell@mths.ca>
2022-02-17 19:46:52 +0000

    laul

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by Peter Gemmell
+# Created on Febuary 2022
+# This line is "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
